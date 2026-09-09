24cfa9c022e25bf8d01e0e55154c9820e5def28e
Praman Patel <patelpraman58@gmail.com>
2026-09-09 23:51:26 +0530

    Add files via upload

---
+++ 1.Iterables & Loops Over Data.py
@@ -0,0 +1,15 @@
+ #iterable is a Python object that contains multiple values that you can go through one by one using a loop....
+
+#Examples: List, tuple, string, dictionary, set....
+
+numbers = [10, 20, 30]
+
+for number in numbers:
+    print(number)
+
+    #Looping over data means going through each item in an iterable one by one using a loop, usually a for loop...
+
+    names = ["Ram", "John", "Aman"]
+
+for name in names:
+    print(name)
