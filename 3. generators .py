f5bfc27bba823b79ec33313306ab0eed91f44bb8
Praman Patel <patelpraman58@gmail.com>
2026-09-12 04:48:36 +0530

    Add files via upload

---
+++ 3. generators .py
@@ -0,0 +1,19 @@
+#A generator is a special type of function that produces values one at a time instead of creating and storing all values in memory at once....
+
+#Generators use the yield keyword...
+
+def complex_calc(i):
+    #some complex calculations here..
+    return i*i
+def get_numbers():
+    for i in range(5):
+        yield complex_calc()   #we'll deal with it later...
+        a = get_numbers()
+        #print(a, type(a))
+
+        print(next(a))
+        print(next(a))
+        print(next(a))
+
+
+
