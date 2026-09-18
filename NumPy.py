ac9329868d4b75301940f1ae9c71c6b26c8e263c
Praman Patel <patelpraman58@gmail.com>
2026-09-18 17:02:32 +0530

    Add files via upload

---
+++ NumPy.py
@@ -0,0 +1,8 @@
+#NumPy (Numerical Python) is a Python library used for working with numbers, arrays, and mathematical operations. It is faster and more convenient than normal Python lists for numerical calculations....
+
+import numpy as np
+
+numbers = np.array([10, 20, 30, 40])
+
+print(numbers)
+print(numbers + 5)
